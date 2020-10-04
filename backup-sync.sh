0c73e80834d7983dcc74ee3a2ba2432bb526fa1c
Tiago Simões <tiago.simoes@uni-foundation.eu>
2020-10-04 23:59:40 +0200

    Added basic support for JSON parsing

--- backup-sync.sh
+++ backup-sync.sh
@@ -2,3 +2,8 @@
 
 # https://www.maketecheasier.com/desktop-notifications-for-linux-command/
 notify-send "Hello $USER" "backup-sync is running" -u normal -i checkbox-checked-symbolic
+
+# load JSON file
+config=$PWD/config.json
+# load variables
+watchdir=`jq '.watchdir' -r < $config`
