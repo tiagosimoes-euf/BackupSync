2c06186f664c62b28c804c1e0650aaa67dd05e79
Tiago Simões <tiago.simoes@uni-foundation.eu>
2020-10-06 23:10:47 +0200

    Refactored to use plain config

--- README.md
+++ README.md
@@ -4,14 +4,30 @@ This is a prototype of a startup utility to sync backup directories.
 
 ## Requirements
 
-    sudo apt install inotify-tools jq
+    sudo apt install inotify-tools rsync
+
+## Installation
+
+    # grab the script
+    git clone https://github.com/tiagosimoes-euf/backup-sync.git
+    # make it executable
+    chmod a+w backup-sync.sh
+    # copy the example config file
+    cp example.backup-sync.cfg backup-sync.cfg
+    # edit the active config file
+    nano backup-sync.cfg
+    # run the script in the background at login
+    echo $PWD/backup-sync.sh >> ~/.profile
+    # profit!
 
 ## Usage
 
-    git clone https://github.com/tiagosimoes-euf/backup-sync.git
-    chmod a+w backup-sync.sh
-    cp example.config.json config.json
+    # go to your source directory under the parent directory being watched
+    cd /WATCHDIR/sourcedir
+    # add a destination directory to a destination file
+    echo /destinationdir >> DESTFILE
+    # any changes in the source will trigger a sync to the destination
 
 ---
 
-Under heavy development...
+Under active development...
--- backup-sync.sh
+++ backup-sync.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
 
-# https://www.maketecheasier.com/desktop-notifications-for-linux-command/
-notify-send "Hello $USER" "backup-sync is running" -u normal -i checkbox-checked-symbolic
+# Import configuration
+source backup-sync.cfg
 
-# load JSON file
-config=$PWD/config.json
-# load variables
-watchdir=`jq '.watchdir' -r < $config`
+if [[ $NOTIFY ]]; then
+  notify-send "Hello $USER" "backup-sync is running in the background" -u normal  -t $TIMEOUT -i checkbox-checked-symbolic
+fi
+
+# Catch changes in the parent directory
+inotifywait -m -r -q --format '%w' -e close_write $WATCHDIR | while read CHANGEPATH
+do
+  # Check for a file with sync destinations
+  if [ -f "$CHANGEPATH$DESTFILE" ]; then
+    mapfile -t lines < $CHANGEPATH$DESTFILE
+    for line in "${lines[@]}"
+    do
+      # Check if it is an actual directory
+      if [[ -d ${line} ]]; then
+        DESTPATH="${line}"
+        rsync -az $CHANGEPATH $DESTPATH --exclude=$DESTFILE
+        if [[ $NOTIFY ]]; then
+          notify-send "backup-sync triggered!" "\nChanged: $CHANGEPATH\n\nUpdated: $DESTPATH" -u normal -t $TIMEOUT -i emblem-synchronizing-symbolic
+        fi
+      fi
+    done
+  fi
+done
