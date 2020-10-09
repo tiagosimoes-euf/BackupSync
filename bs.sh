3faf1a809a84cd3d16ee104ef2dcef43cef53158
Tiago Simões <tiago.simoes@uni-foundation.eu>
2020-10-10 01:14:44 +0200

    Quick cleanup on the main script

--- bs.sh
+++ bs.sh
@@ -3,24 +3,25 @@
 # Import configuration
 source backup-sync.cfg
 
-if [[ $NOTIFY ]]; then
-  notify-send "Hello $USER" "backup-sync is running in the background" -u normal  -t $TIMEOUT -i checkbox-checked-symbolic
+if [[ ${NOTIFY} ]]; then
+  notify-send "Hello ${USER}" "backup-sync is running in the background" -u normal  -t ${TIMEOUT} -i checkbox-checked-symbolic
 fi
 
 # Catch changes in the parent directory
-inotifywait -m -r -q --format '%w' -e close_write $WATCHDIR | while read CHANGEPATH
+inotifywait -m -r -q --format '%w' -e close_write ${WATCHDIR} | while read CHANGEPATH
 do
   # Check for a file with sync destinations
-  if [ -f "$CHANGEPATH$DESTFILE" ]; then
-    mapfile -t lines < $CHANGEPATH$DESTFILE
+  if [ -f "${CHANGEPATH}${DESTFILE}" ]; then
+    mapfile -t lines < ${CHANGEPATH}${DESTFILE}
     for line in "${lines[@]}"
     do
       # Check if it is an actual directory
       if [[ -d ${line} ]]; then
         DESTPATH="${line}"
-        rsync -az $CHANGEPATH $DESTPATH --exclude=$DESTFILE
-        if [[ $NOTIFY ]]; then
-          notify-send "backup-sync triggered!" "\nChanged: $CHANGEPATH\n\nUpdated: $DESTPATH" -u normal -t $TIMEOUT -i emblem-synchronizing-symbolic
+        # Sync everything to the destination except for the file with the sync destinations
+        rsync -az ${CHANGEPATH} ${DESTPATH} --exclude=${DESTFILE}
+        if [[ ${NOTIFY} ]]; then
+          notify-send "backup-sync triggered!" "\nChanged: ${CHANGEPATH}\n\nUpdated: ${DESTPATH}" -u normal -t ${TIMEOUT} -i emblem-synchronizing-symbolic
         fi
       fi
     done
